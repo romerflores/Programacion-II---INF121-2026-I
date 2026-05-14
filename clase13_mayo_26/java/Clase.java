52c62f0377aa12fabcea5a3b1b4b56fe991f8efc
Romer <romer141016@gmail.com>
2026-05-14 12:41:00 -0400

    clase 13 de mayo interfaz grafica

---
+++ clase13_mayo_26/java/Clase.java
@@ -0,0 +1,23 @@
+
+import ui.Pantalla1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Romer
+ */
+public class Clase {
+
+    public static void main(String[] args) {
+        
+        Pantalla1 p1=new Pantalla1();
+        p1.setVisible(true);
+//        p1.setSize(1000, 200);
+//        p1.setLocation(0, 0);
+        p1.setLocationRelativeTo(null);
+        
+    }
+}
